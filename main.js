32dca8a3fe19e15fdfe48309397bfc1f04fa4c74
Carlos Saraiva <carlos.saraiva@msn.com>
2015-10-22 16:41:08 -0200

    Express added to the project.

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 var gith = require('gith').create(9001);
+var express = require('express');
+var app = express();
+var messages = [];
+
+app.get('/', function (req, res) {
+    res.json(messages);
+});
+
+var server = app.listen(80, funcion() {
+    console.log("Server started!");
+})
 
 gith({
     repo: 'carlossaraiva/hellograph'
 }).on('all', function (payload) {
+    messages.push(payload);
     console.log('Post-receive happened!');
 });
