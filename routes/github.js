c470c7201626b12bebf9f4fd38b99e2d1040c07c
Carlos Saraiva <carlos.saraiva@msn.com>
2015-10-24 01:37:38 -0200

    Refactoring

---
+++ routes/github.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var Event = require('../models/event.js');
+var cotacao;
+var githubMiddleware = require('github-webhook-middleware')({
+    secret: '1234'
+});
+
+router.post('/', githubMiddleware, function (req, res) {
+    var event = new Event({
+        action: req.headers['x-github-event'],
+        request: req.body,
+    });
+    event.save();
+    io.emit('newItem', {});
+    console.debug(event);
+    res.send("New " + event.action + " has been added!");
+});
+
+module.exports = router;
