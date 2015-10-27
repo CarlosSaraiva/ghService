461ef2fe065b3472471ed641f7a954a7890fe906
Carlos Saraiva <carlos.saraiva@msn.com>
2015-10-27 21:53:05 -0200

    Update

--- models/event.js
+++ models/event.js
@@ -5,4 +5,22 @@ var eventSchema = new Schema({}, {
     strict: false
 });
 
+eventSchema.statics.groupBy = function groupBy(key, callback) {
+    this.aggregate([{
+        $group: {
+            _id: '$action',
+            count: {
+                $sum: 1
+            }
+        }
+    }], function (err, result) {
+        if (err) {
+            console.log(err);
+        } else {
+            callback(result);
+        }
+    });
+
+};
+
 module.exports = mongo.model('Event', eventSchema);
