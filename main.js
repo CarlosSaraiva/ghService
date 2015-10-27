376bfcf2483f3657910a8bb1192ffb53b862de5d
Carlos Saraiva <carlos.saraiva@msn.com>
2015-10-27 18:56:19 -0200

    Update

--- main.js
+++ main.js
@@ -27,4 +27,24 @@ io.on('connection', function (socket) {
         timestamp: socket.handshake.time
     });
     console.log("Usuário conectado!");
+
+    var count = (function () {
+        var db = Event.aggregate([{
+            $group: {
+                _id: '$action',
+                count: {
+                    $sum: 1
+                }
+            }
+        }], function (err, result) {
+            if (err) {
+                io.emit('onError', err);
+            } else {
+                io.emit('onDbCount', {
+                    groupBy: result
+                });
+                console.log('onDbCount triggered');
+            }
+        });
+    })();
 });
