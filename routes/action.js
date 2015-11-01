0494ddbdcd67c82f24ef81366ac6af848273580b
Carlos Saraiva <carlos.saraiva@msn.com>
2015-11-01 05:50:15 -0200

    Fixed problem with circular json using decycle.
    Signed-off-by: Carlos Saraiva <carlos.saraiva@msn.com>

--- routes/action.js
+++ routes/action.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Event = require('../models/event.js');
+var j = require('cycle');
 
 router.get('/counter', function (req, res) {
     Event.countEventsGroups()
         .then(function (counter) {
-            res.json(JSON.stringify(counter));
+            console.log(j);
+            res.json(JSON.stringify(j.decycle(counter)));
         })
         .catch(function (err) {
             console.log(err);
