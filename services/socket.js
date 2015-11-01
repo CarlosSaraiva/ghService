f4693fff65694381c44894b81388a5fbecd16a2b
Carlos Saraiva <carlos.saraiva@msn.com>
2015-11-01 06:12:43 -0200

    io emit flow updated
    Signed-off-by: Carlos Saraiva <carlos.saraiva@msn.com>

--- services/socket.js
+++ services/socket.js
@@ -4,25 +4,25 @@ module.exports = function (server) {
     var io = require("socket.io")(server);
     var listeners = [];
 
-    io.emitIO = function (event, data) {
+    io.event = function (event, data) {
         return new Promise(function (resolve, reject) {
-            io.emit(event, JSON.stringify(data));
+            io.emit(event, JSON.stringify(JSON.decycle(data)));
             console.log('Event \'' + event + '\' emited.');
             resolve(true);
         });
     };
 
-    io.on('teste', function (data) {
-        console.log('Event \'' + 'test' + '\' received.');
-    });
+    // io.on('teste', function (data) {
+    //     console.log('Event \'' + 'test' + '\' received.');
+    // });
 
-    io.on('onnewrequest', function (data) {
-        console.log('Event \'' + 'onnewrequest' + '\' received.');
-    });
+    // io.on('onnewrequest', function (data) {
+    //     console.log('Event \'' + 'onnewrequest' + '\' received.');
+    // });
 
-    io.on('ondbgroupby', function (data) {
-        console.log('Event \'' + 'ondbgroupby' + '\' received.');
-    });
+    // io.on('ondbgroupby', function (data) {
+    //     console.log('Event \'' + 'ondbgroupby' + '\' received.');
+    // });
 
     io.on('connection', function (socket) {
         'use strict';
